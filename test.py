38a1fa54f0741a5d5ddf8c8383760f560cd971c5
yk52 <yumikaku@web.de>
2019-11-21 14:04:48 +0100

    added typical values to test.py in order of array

--- test.py
+++ test.py
@@ -5,7 +5,21 @@ test script for scriptConverter
 
 import scriptConverter
 
-params = ["testRaster", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1]
+x = 10
+y = 10
+z = 10
+startLeistung = -26
+pulse = 1
+repRate = 20 
+PulseEnergy = 220
+hv = 26
+EnergyMode = 0
+TriggerMode = 0
+waitMs = 100
+pitch = 1
+
+params = ["testRaster", x, y, z, startLeistung, pulse, repRate, PulseEnergy, hv,
+        EvergyMode, TriggerMode, waitMs, pitch]
 queue = [[0,0],[1,0],[1,1],[1,2],[1,3],[0,1]]
 p = [[0,0],[4,2]]
 l = [[0,0,2,0],[2,0,2,3],[2,3,1,3],[1,3,1,2]]
